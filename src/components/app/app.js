da725c658a8d2e3063f3b44d30172173a2c7a7a3
maxbendas <56072735+maxbendas@users.noreply.github.com>
2020-11-05 20:43:34 +0200

    12-10

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,10 +3,12 @@ import './app.css'
 import {Route, Switch} from "react-router-dom";
 import HomePage from "../pages/home-page";
 import CartPage from "../pages/cart-page";
+import ShopHeader from "../shop-header";
 
 const App = () => {
     return (
         <main className="container">
+            <ShopHeader numItems={5} total={200}/>
             <Switch>
                 <Route path='/' component={HomePage} exact/>
                 <Route path='/cart' component={CartPage}/>
--- src/services/bookstore-services.js
+++ src/services/bookstore-services.js
@@ -4,13 +4,16 @@ export default class BookstoreService {
             {
                 id:1,
                 title:'JScript',
-                author:'Fowler'
+                author:'Fowler',
+                price:32,
+                coverImage: 'https://covers.zlibcdn2.com/covers/books/1c/b3/90/1cb390e6ff7d11dd1a92c92a2ae324ba.jpg'
             },
             {
                 id:2,
                 title:'React',
-                author:'Nygard'
-            }
+                author:'Nygard',
+                price:45,
+                coverImage: 'https://covers.zlibcdn2.com/covers/books/78/72/74/7872743894e432d99205d370e86ceeae.jpg'            }
         ]
     }
 }
